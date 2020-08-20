6e8a56c7f5ecc6a7ac6f12b024edd19a96553b96
phaneendra kulkarni <68918128+kulkarni90@users.noreply.github.com>
2020-08-20 17:40:57 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,7 @@
+public class Devops{
+            public static void main(string[ ] args){
+	             for (int i=1;i<=3;i++){
+                     system.out.println("Hello Devops team"+i);
+	            }
+         }
+}	
